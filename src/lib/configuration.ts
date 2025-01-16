15001a8a75cde56b99f1a691479bd9015fbb27aa
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2025-01-16 12:56:38 +0530

    fix: Update environment variable name in .env.example and configuration file to NEXT_PUBLIC_LANGGRAPH_API_URL

--- src/lib/configuration.ts
+++ src/lib/configuration.ts
@@ -1,12 +1,12 @@
 'server only'
 export const configuration = {
-  langgraphApiUrl: process.env.LANGGRAPH_API_URL,
+  langgraphApiUrl: process.env.NEXT_PUBLIC_LANGGRAPH_API_URL,
   langgraphId: 'agent',
   assistantName: 'assistant',
 }
 console.log(
-  '🚀 ~ export  configuration.process.env.LANGGRAPH_API_URL:',
-  process.env.LANGGRAPH_API_URL,
+  '🚀 ~ export  configuration.process.env.NEXT_PUBLIC_LANGGRAPH_API_URL:',
+  process.env.NEXT_PUBLIC_LANGGRAPH_API_URL,
   'configuration',
   configuration.langgraphApiUrl
 )
